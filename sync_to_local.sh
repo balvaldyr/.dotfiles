b3928361cf62e65c8eca7defa74e7222365826ff
Alexander Kane <alexander.kane@daqri.com>
2019-12-11 11:16:03 +0100

    sync to local works

--- sync_to_local.sh
+++ sync_to_local.sh
@@ -4,4 +4,34 @@
 # tag file containing the sha of this commit.
 
 echo $HOME
+git submodule sync
+git submodule update
 
+input="./fileList.txt"
+while IFS= read -r line
+do
+  echo "link $PWD/$line ~/$line"
+  if [[ -f ~/$line ]]; then
+    if [[ ! -f ~/$line.bk ]]; then
+      echo "Making backup of ~/$line"
+      mv ~/$line ~/$line.bk
+    else
+      rm ~/$line
+    fi
+
+  elif [[ -d ~/$line ]]; then
+    if [[ ! -d ~/$line.bk ]]; then
+      echo "Making backup of ~/$line"
+      mv ~/$line ~/$line.bk
+    else
+      rm -rf ~/$line
+    fi
+  elif [[ -L ~/$line ]]; then
+    rm ~/$line
+  fi
+
+  ln -sfnv $PWD/$line ~/$line
+done < "$input" 
+
+# can't use a symlink for .gitconfig
+cp .gitconfig ~/.gitconfig
